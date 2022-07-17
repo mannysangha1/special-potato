aac8bdc148ca86bf0338c8dbdc5d4f2857f1b1bd
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-07-17 06:50:53 -0700

    final changes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,39 +1,44 @@
-// Getting the DOM Eleements
-// DOM for results
-const resultDOM = document.getElementById('result');
-// DOM for copy
-const copybtnDOM = document.getElementById('copy');
-// Dom for length
-const lengthDOM = document.getElementById('length');
-// DOM for uppercase
-const uppercaseDOM = document.getElementById('uppercase');
-// DOM for numbers
-const numbersDOM = document.getElementById('numbers');
-// DOM for symbols
-const symbolsDOM = document.getElementById('symbols');
-// DOM for generate 
-const generatebtn = document.getElementById('generate');
-// DOM for generate form
-const form = document.getElementById('passwordGeneratorForm');
+// Get DOM results
+// RESULTS
+var resultDOM = document.getElementById('result');
+// COPY
+var copybtnDOM = document.getElementById('copy');
+// LENGHT OF PASSWORD
+var lengthDOM = document.getElementById('length');
+// UPPERCASE LETTERS
+var uppercaseDOM = document.getElementById('uppercase');
+// NUMBERS
+var numbersDOM = document.getElementById('numbers');
+// SYMBOLS
+var symbolsDOM = document.getElementById('symbols');
+// GEBERATE THE PASSWORD
+var generatebtn = document.getElementById('generate');
+// PASSWORD FORM
+var form = document.getElementById('passwordGeneratorForm');
 
-// Generates codes
-const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
-const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
-const NUMBER_CODES = arrayFromLowToHigh(48, 57);
-const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+// Generating Character Codes For The Application 
+// USE THE ASCII TABLE TO GENERATE CHARACTERS
+// UPPERCASE CHARACTERS 65-90 A-Z
+var UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+// LOWER CASE CHARACTERS 97-122 a-z
+var LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+// NUMBER CHARACTERS 48-57 0-9
+var NUMBER_CODES = arrayFromLowToHigh(48, 57);
+// SYMBOL CHARACTERS// SPECIAL CHARACTERS 
+var SYMBOL_CODES = arrayFromLowToHigh(33, 47)
   .concat(arrayFromLowToHigh(58, 64))
   .concat(arrayFromLowToHigh(91, 96))
   .concat(arrayFromLowToHigh(123, 126));
 
-// Code for copy password button
-copybtnDOM.addEventListener('click', () => {
-  const textarea = document.createElement('textarea');
-  const passwordToCopy = resultDOM.innerText;
+// Copy Password
+copybtnDOM.addEventListener("click",() => {
+  var textarea = document.createElement('textarea');
+  var passwordToCopy = resultDOM.innerText;
 
-  // When Password is Empty
+  // Edge Case when Password is Empty
   if (!passwordToCopy) return;
 
-  // Copy Functiona
+  // Copy Functionality
   textarea.value = passwordToCopy;
   document.body.appendChild(textarea);
   textarea.select();
@@ -42,14 +47,14 @@ copybtnDOM.addEventListener('click', () => {
   alert('Password Copied to Clipboard');
 });
 
-// Checking the Options that are selected and setting the password
+// Checking the options that are selected and setting the password
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const characterAmount = lengthDOM.value;
-  const includeUppercase = uppercaseDOM.checked;
-  const includeNumbers = numbersDOM.checked;
-  const includeSymbols = symbolsDOM.checked;
-  const password = generatePassword(
+  var characterAmount = lengthDOM.value;
+  var includeUppercase = uppercaseDOM.checked;
+  var includeNumbers = numbersDOM.checked;
+  var includeSymbols = symbolsDOM.checked;
+  var password = generatePassword(
     characterAmount,
     includeUppercase,
     includeNumbers,
@@ -59,30 +64,38 @@ form.addEventListener('submit', (e) => {
 });
 
 // The Password Generating Function
-let generatePassword = (
+// FUNTION FUNCTION FUNCTION
+var generatePassword = (
   characterAmount,
   includeUppercase,
   includeNumbers,
   includeSymbols
 ) => {
-  let charCodes = LOWERCASE_CODES;
+  var charCodes = LOWERCASE_CODES;
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CODES);
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CODES);
+  
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CODES);
-  const passwordCharacters = [];
-  for (let i = 0; i < characterAmount; i++) {
-    const characterCode =
+  var passwordCharacters = [];
+  // MATH EQUATIONS ARE FUN
+  for (var i = 0; i < characterAmount; i++) {
+    var characterCode =
       charCodes[Math.floor(Math.random() * charCodes.length)];
     passwordCharacters.push(String.fromCharCode(characterCode));
   }
   return passwordCharacters.join('');
 };
-
+//FUNCTIONS FOR FUN
+console.log("What's the easiest password to crack?");
 // The Character Code Generating Function
+console.log("Password");
+console.log("123456789")
+console.log("BAD JOKES INC.")
 function arrayFromLowToHigh(low, high) {
-  const array = [];
-  for (let i = low; i <= high; i++) {
+  var array = [];
+  for (var i = low; i <= high; i++) {
     array.push(i);
   }
   return array;
+  
 }
